8bdd6cec4bf3d0ef190c2d88af2b903ca6601c89
JakeAve <jacobjavery@gmail.com>
2023-09-22 00:01:41 -0600

    Created back out on installer

--- scripts/uninstall.ts
+++ scripts/uninstall.ts
@@ -5,29 +5,26 @@ const MAIN_PATH = resolve(HOME, ".self_encrypt_🔐");
 
 const CONFIRMATION_TEXT = "destroy my keys";
 
-async function uninstall() {
-  const shouldProceed = await Input.prompt(
-    colors.brightYellow(
-      `Uninstalling will delete all of the keys associated with self_encrypt. If you want to proceed type "${CONFIRMATION_TEXT}" Would you like to proceed?`,
-    ),
-  );
-
-  if (shouldProceed !== CONFIRMATION_TEXT) {
-    console.log(
-      colors.brightYellow("Halting uninstall. Nothing was uninstalled."),
-    );
-    return;
-  }
+export async function uninstall() {
   try {
     await Deno.remove(MAIN_PATH, { recursive: true });
     console.log(colors.brightGreen("Successfully uninstalled"));
   } catch (err) {
     console.error(
-      colors.brightYellow(`The uninstall failed because of ${err}`),
+      colors.brightYellow(`The uninstall failed because of ${err}`)
     );
   }
 }
 
 if (import.meta.main) {
+  const shouldProceed = await Input.prompt(
+    colors.brightYellow(
+      `Uninstalling will delete all of the keys associated with self_encrypt. If you want to proceed type "${CONFIRMATION_TEXT}" Would you like to proceed?`
+    )
+  );
+
+  if (shouldProceed !== CONFIRMATION_TEXT) {
+    throw new Error("Halting uninstall. Nothing was deleted.");
+  }
   uninstall();
 }
